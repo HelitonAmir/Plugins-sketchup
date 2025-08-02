d122f6b1eeb1a0484f7015e6c36d2711fee8a822
Hamir <hamir.br@gmail.com>
2025-08-02 11:51:04 -0300

    ajustes nos ícones

--- src/hamir_modulos_marcenaria/modulos_marcenaria/main.rb
+++ src/hamir_modulos_marcenaria/modulos_marcenaria/main.rb
@@ -6,16 +6,31 @@ require_relative 'cabinet_box_tool'
 module HamirTools
   module ModulosMarcenaria
 
-    unless file_loaded?(__FILE__)
-      menu = UI.menu("Plugins")
-      menu.add_item("Criar caixote") {
-        HamirTools::ModulosMarcenaria.create_module("CabinetBox")
-      }
-      file_loaded(__FILE__)
-    end
-
-    def self.create_module(tipo)
+    def self.create_module
       Sketchup.active_model.select_tool(HamirTools::ModulosMarcenaria::CabinetBoxTool.new)
     end
+
+    def self.build_command(icon, tooltip, action)
+      icon_path = File.join(__dir__, "icons", "#{icon}.svg")
+      cmd = UI::Command.new(tooltip, &action.to_proc)
+      cmd.tooltip = tooltip
+      cmd.small_icon = icon_path
+      cmd.large_icon = icon_path
+      cmd
+    end
+
+    unless file_loaded?(__FILE__)
+      #menu = UI.menu("Plugins")
+      #menu.add_item("Criar caixote") {
+      #  HamirTools::ModulosMarcenaria.create_module("CabinetBox")
+      #}
+      #file_loaded(__FILE__)
+      toolbar = ::UI::Toolbar.new("Marcenaria")
+      toolbar.add_item(self.build_command("icon_cabinet_box", "Criar Caixote", proc {HamirTools::ModulosMarcenaria.create_module}))
+      toolbar.add_item(self.build_command("icon_doors", "Criar Portas", proc {HamirTools::ModulosMarcenaria.create_module}))
+      toolbar.add_item(self.build_command("icon_drawers", "Criar Gavetas", proc {HamirTools::ModulosMarcenaria.create_module}))
+    end
+
+
   end # module ModulosMarcenaria
 end # module HamirTools
